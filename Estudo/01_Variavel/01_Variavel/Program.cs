38bd3ad9a6ba3885064f3dc9ee9227514d771daa
Fernando Pereira <fnd.alemao@hotmail.com>
2018-05-08 14:02:39 -0300

    Primeira aula de variaveis.

---
+++ Estudo/01_Variavel/01_Variavel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Variavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte meuByte;
+            System.Byte meuByte2;
+
+            meuByte = 9;
+
+            sbyte meuSByte = -128;
+
+        }
+    }
+}
