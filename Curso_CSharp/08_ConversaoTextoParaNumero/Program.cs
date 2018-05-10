e4c59e174d7ec8b5a9c50b6b2c05ed3055843ebb
Fernando Pereira <fnd.alemao@hotmail.com>
2018-05-10 13:56:52 -0300

    Curso C#, adicionado aula 08 sobre conversão de texto para numero.

---
+++ Curso_CSharp/08_ConversaoTextoParaNumero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ConversaoTextoParaNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NumeroTexto = "30";
+            int Numero1 = int.Parse(NumeroTexto);
+            int Numero2 = 30;
+
+            Console.Write("O resultado é: " + (Numero1 + Numero2));
+            Console.ReadKey();
+
+        }
+    }
+}
--- Curso_CSharp/08_ConversaoTextoParaNumero/Properties/AssemblyInfo.cs
+++ Curso_CSharp/08_ConversaoTextoParaNumero/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("01_Variavel")]
+[assembly: AssemblyTitle("08_ConversaoTextoParaNumero")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01_Variavel")]
+[assembly: AssemblyProduct("08_ConversaoTextoParaNumero")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("0152a25a-404c-47b3-944c-4f1062d220c9")]
+[assembly: Guid("ab4f2cb3-988f-49fa-b26e-59f1c1e724be")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
